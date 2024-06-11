04691578e67bc123a99ec543c80adab3bd271deb
MaximeChab <maximechab@outlook.fr>
2024-06-11 12:05:32 +0200

    Login page

--- PR3-Blazor/Components/Services/UtilisateurService.cs
+++ PR3-Blazor/Components/Services/UtilisateurService.cs
@@ -1,6 +1,10 @@
 ﻿using Newtonsoft.Json;
 using PR3_Blazor.Components.Models;
 using System.Text;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
 
 namespace PR3_Blazor.Components.Services
 {
@@ -54,6 +58,40 @@ namespace PR3_Blazor.Components.Services
             var response = await _httpClient.DeleteAsync($"http://localhost:5252/api/Utilisateurs/{utilisateurId}");
             response.EnsureSuccessStatusCode();
 
+        }
+
+        public async Task<string> UseExist(LoginRequest loginRequest)
+        {
+            loginRequest.Login = "admin";
+            loginRequest.MotDePasse = "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918";
+
+            var response = await _httpClient.PostAsJsonAsync("api/Utilisateurs/Exist", loginRequest);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadFromJsonAsync<JwtResponse>();
+                // Optionally store the token, e.g., in local storage or a service
+                // await _localStorage.SetItemAsync("authToken", result.Token);
+                return result.Token;
+            }
+            else
+            {
+                // Handle the case where the user does not exist or other errors
+                // You might want to throw an exception or return a specific result
+                return null;
+            }
+        }
+
+
+        public class LoginRequest
+        {
+            public string Login { get; set; }
+            public string MotDePasse { get; set; }
+        }
+
+        public class JwtResponse
+        {
+            public string Token { get; set; }
         }
     }
 }
